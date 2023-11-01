d068f77351d896c29428c20b030d864240570a56
Navarroseb <sebastnavarro@gmail.com>
2023-10-31 21:50:28 -0300

    components & props

---
+++ src/components/Gretting.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Gretting = (props) => {
+    
+    
+
+    return (
+        <>
+            <h1>HOLA {props.text}
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus amet, animi, libero nobis ipsam quidem fugiat esse provident atque odio possimus deserunt adipisci nemo dolor quibusdam officia maiores, consequuntur iure.</p>
+            </h1>
+        </>
+    )
+}
+
+export default Gretting;
