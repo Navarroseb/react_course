309112175124851c1a5b97879620da822d5347b5
Navarroseb <sebastnavarro@gmail.com>
2023-11-02 21:24:25 -0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Gretting from './components/Gretting'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Gretting text="Hola Sebastian" isBigger={true}/>        
+     <Gretting text="Hola Sebastian" isBigger={true}/>     
+     <Button text="Click me" />
+     <Gretting text="Hello World">
+        <hr />
+        <strong>I'm a children tag</strong>
+        <h5>Children h5</h5>
+      </Gretting>   
     </>
   )
 }
--- src/components/Gretting.jsx
+++ src/components/Gretting.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const Gretting = (props) => {
-    
-    
+const Gretting = ({ text, children }) => {
+    console.log(children);
+
 
     return (
         <>
-            <h1>HOLA {props.text}
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus amet, animi, libero nobis ipsam quidem fugiat esse provident atque odio possimus deserunt adipisci nemo dolor quibusdam officia maiores, consequuntur iure.</p>
+            <h1>
+                {text}          
             </h1>
+            {children}
         </>
     )
 }
