8dc8a8180c2d6115f94c8412edd112b0114ae52b
Navarroseb <sebastnavarro@gmail.com>
2023-11-07 22:03:38 -0300

    Constructor & Super

---
+++ src/components/ejemplo-classes.js
@@ -0,0 +1,23 @@
+class Persona {
+    constructor(name, surname) {
+        this.firstName = name;
+        this.lastName = surname;
+
+    }
+    saludar () {
+        return `¿Hola qué tal?, mi nombre es ${this.firstName} ${this.lastName}`;
+    }
+}
+
+class Estudiante extends Persona {
+    constructor(name, surname, email){
+        super(name, surname)
+        this.email = email;
+    }
+    irAEstudiar(nombreDelCurso){
+        return `Hola, soy ${this.firstName} y estoy yendo al curso de ${nombreDelCurso} `
+    }
+}
+
+const sebas = new Estudiante("Sebas", "Navarro", "sebasnavarro@asd.com")
+console.log(sebas)
