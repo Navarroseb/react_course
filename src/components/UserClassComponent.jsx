2e2ffe8a23e2302b4cfd63afe7692c4ded667f1d
Navarroseb <sebastnavarro@gmail.com>
2023-11-11 22:49:48 -0300

    Api REST with Class Component

--- src/components/UserClassComponent.jsx
+++ src/components/UserClassComponent.jsx
@@ -1,25 +1,39 @@
 import { Component } from "react";
+import UserData from "./UserData";
+
 
 class UserClassComponent extends Component {
+    constructor(){
+        super();
+        this.state = {
+            usersList: [],
+            isLoading: true
+        }
+    }
     componentDidMount() {
         (async () => {
-            const API_URL = 'https://reqres.in/api/users?per_page=20'
-            const temp = await fetch(API_URL).then(response => response.json())
-            console.log(temp)
+            const API_URL = 'https://reqres.in/api/users?per_page=2&page=1'
+            const { data } = await fetch(API_URL).then(response => response.json())
+            this.setState({
+                 usersList: data,
+                 isLoading: false
+            });
+           
         })();
       
     }
 
     render() {
+        const {isLoading, usersList} = this.state;
         return (
             <>
               <h3>Users List</h3>
-              {/* {usersList.length === 0 && <h4>No hay ususarios para mostrar</h4>}
-              {usersList.length > 0 && (
+               {isLoading && <h4>Cargando...</h4>}
+               {!isLoading && (
                 <ul>
                   {usersList.map(user => <UserData oneUser={user} />)}
                 </ul>
-              )} */}
+              )}  
         
             </>
           )
--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -5,6 +5,8 @@ const UserData = ({ oneUser }) => {
     <>
     <li key={oneUser.id}>
                 <h3>{`${oneUser.id} - ${oneUser.first_name} ${oneUser.last_name}`}</h3>
+                <img src={oneUser.avatar}  />
+                <br />
                 <a href={`mailto:${oneUser.email}`}>{oneUser.email}</a>
             </li>
     </>
