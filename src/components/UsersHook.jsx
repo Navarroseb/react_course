3c6c425bb8b7a3370e9aa5c69240130d451fc7c6
Navarroseb <sebastnavarro@gmail.com>
2023-11-11 23:44:03 -0300

    useEffect

--- src/components/UsersHook.jsx
+++ src/components/UsersHook.jsx
@@ -1,15 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import UserData from "./UserData";
 
 const UsersHook = () => {
     const [usersList, setUsersList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
+    const [value, setValue] = useState(0);
+
+
+    useEffect(() => {
+        (async () => {
+            const API_URL = 'https://reqres.in/api/users?per_page=2&page=1'
+            const { data } = await fetch(API_URL).then(response => response.json())
+            setUsersList(data);
+            setIsLoading(false);
+                      
+        })();
+    }, []);
+
+    
     
     return (
         <>
             <h3>Users List</h3>
+            
             {isLoading && <h4>Cargando...</h4>}
+
                {!isLoading && (
                 <ul>
                   {usersList.map(user => <UserData oneUser={user} />)}
