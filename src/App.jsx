714fffd56d9618ba79b5c9dc774a36d145a60a40
Navarroseb <sebastnavarro@gmail.com>
2023-11-15 22:51:54 -0300

    Adding TodoList Folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,11 @@ import UserClassComponent from './components/UserClassComponent';
 import UsersHook from './components/UsersHook';
 
 
-/* import USERS_DATA from "./data//users.json";
-import Users from './components/Users';
-import Counter from './components/Counter'; */
-
-
-
 function App() {
     return (
     <>
      <Gretting text={"Hola!"}/>
-     <UsersHook />
-     {/* <Counter subject="React" developerName="Jane"/>
-     <Users usersList={USERS_DATA} /> */}
+     
     </>
   );
 }
