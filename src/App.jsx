ea5e30d41bdf40358150a233b70da16951a34419
Navarroseb <sebastnavarro@gmail.com>
2023-11-15 22:54:37 -0300

    Adding Form to TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 import Gretting from './components/Gretting'
 import UserClassComponent from './components/UserClassComponent';
 import UsersHook from './components/UsersHook';
+import Form from './components/TodoList/Form';
 
 
 function App() {
     return (
     <>
      <Gretting text={"Hola!"}/>
-     
+     <Form/>
     </>
   );
 }
